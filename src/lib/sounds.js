b3e9d4c9609191200a62bad59f6e8e52a9e9f5ed
Brunda reddy <ybrundareddy2006@gmail.com>
2026-05-14 18:10:38 +0530

    Update sounds.js

--- src/lib/sounds.js
+++ src/lib/sounds.js
@@ -2,7 +2,9 @@ let ctx = null;
 
 function ac() {
   if (!ctx) {
-    const AC = window.AudioContext || window.webkitAudioContext;
+    const AC =
+      window.AudioContext ||
+      window.webkitAudioContext;
 
     if (!AC) return null;
 
@@ -12,7 +14,12 @@ function ac() {
   return ctx;
 }
 
-function beep(freq, duration = 0.12, type = "square", volume = 0.15) {
+function beep(
+  freq,
+  duration = 0.12,
+  type = "square",
+  volume = 0.15
+) {
   const a = ac();
 
   if (!a) return;
@@ -41,41 +48,9 @@ function beep(freq, duration = 0.12, type = "square", volume = 0.15) {
 }
 
 export const sfx = {
-  tick: () => beep(660, 0.08, "square", 0.12),
+  tick: () =>
+    beep(660, 0.08, "square", 0.12),
 
-  shoot: () => beep(880, 0.22, "sawtooth", 0.2),
-
-  win: () => {
-    beep(523, 0.1);
-
-    setTimeout(() => beep(659, 0.1), 110);
-
-    setTimeout(() => beep(784, 0.18), 220);
-  },
-
-  lose: () => {
-    beep(330, 0.14, "sawtooth", 0.18);
-
-    setTimeout(() => beep(220, 0.22, "sawtooth", 0.18), 150);
-  },
-
-  tie: () => beep(440, 0.16, "triangle", 0.15),
-
-  matchWin: () => {
-    [523, 659, 784, 1046].forEach((f, i) =>
-      setTimeout(
-        () => beep(f, 0.16, "square", 0.18),
-        i * 120
-      )
-    );
-  },
-
-  matchLose: () => {
-    [392, 330, 262, 196].forEach((f, i) =>
-      setTimeout(
-        () => beep(f, 0.2, "sawtooth", 0.18),
-        i * 150
-      )
-    );
-  },
+  shoot: () =>
+    beep(880, 0.22, "sawtooth", 0.2),
 };
